f3f80216e1b6a0960ed60a5e1844f43a7ddf7947
bagayan0000-cell <305433968+bagayan0000-cell@users.noreply.github.com>
2026-07-18 00:57:18 +0530

    update spin

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const prize = document.getElementById("prize");
 const resultText = document.getElementById("resultText");
 const couponCode = document.getElementById("couponCode");
 const closePopup = document.getElementById("closePopup");
-
+const claimBtn = document.getElementById("claimBtn");
 const offers = [
 "✂️ Free Hair Cut",
 "🎁 10% OFF",
@@ -35,7 +35,29 @@ setTimeout(() => {
 
 prize.innerHTML = offers[random];
 resultText.innerHTML = "🎉 You Won";
-couponCode.innerHTML = "Coupon: GX" + Math.floor(1000 + Math.random() * 9000);
+
+const coupon = "GX" + Math.floor(1000 + Math.random() * 9000);
+
+couponCode.innerHTML = "Coupon: " + coupon;
+
+const message =
+`Hello Grooming Xpress 👋
+
+I won:
+
+${offers[random]}
+
+Coupon Code:
+${coupon}
+
+I'd like to claim my offer.`;
+
+claimBtn.onclick = function () {
+    window.open(
+        "https://wa.me/+91 81003 45294?text=" + encodeURIComponent(message),
+        "_blank"
+    );
+};
 
 popup.style.display = "flex";
 const claimBtn = document.getElementById("claimBtn");
